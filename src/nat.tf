1beb791cf5717a41ad3162a227e481e91c5f71e5
ehime <dodomeki@gmail.com>
2018-08-17 10:59:50 -0700

    nats configuration for ge-health and vpc/ssh combo

---
+++ src/nat.tf
@@ -0,0 +1,41 @@
+/** nat instance */
+resource "aws_instance" "nat" {
+  ami               = "${lookup(var.amis, var.region)}"
+  availability_zone = "${var.default_az}"
+  instance_type     = "t2.micro"
+
+  vpc_security_group_ids = [
+    "${aws_security_group.ge.id}",
+    "${aws_security_group.vpc_nat.id}",
+    "${aws_security_group.ssh.id}",
+  ]
+
+  subnet_id                   = "${aws_subnet.public.id}"
+  associate_public_ip_address = true
+  source_dest_check           = false
+  key_name                    = "${aws_key_pair.tf-deployment.key_name}"
+
+  provisioner "remote-exec" {
+    connection {
+      user        = "ubuntu"
+      timeout     = "5m"
+      private_key = "${file("ssh/tf-deployment.pem")}"
+    }
+
+    inline = [
+      "sudo apt -y update",
+      "sudo apt -y upgrade",
+      "sudo iptables -t nat -A POSTROUTING -j MASQUERADE",
+      "echo 1 |sudo tee /proc/sys/net/ipv4/conf/all/forwarding >/dev/null",
+    ]
+  }
+
+  tags {
+    Name = "VPC NAT"
+  }
+}
+
+resource "aws_eip" "nat" {
+  instance = "${aws_instance.nat.id}"
+  vpc      = true
+}
