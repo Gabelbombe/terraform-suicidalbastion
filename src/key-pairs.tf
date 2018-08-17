94d87d93f7e3ab8a9f5bf630ada0813bb235c32b
ehime <dodomeki@gmail.com>
2018-08-17 12:54:06 -0700

    more heartache

--- src/key-pairs.tf
+++ src/key-pairs.tf
@@ -1,11 +1,11 @@
 /** key for deployment of jumpbox and nat */
 resource "aws_key_pair" "tf-deployment" {
   key_name   = "tf-deployment"
-  public_key = "${file("ssh/tf-deployment.pub")}"
+  public_key = "${file("~/.ssh/tf-deployment.pub")}"
 }
 
-/** key for ge */
-resource "aws_key_pair" "ge" {
-  key_name   = "ge-jumpbox"
-  public_key = "${file("ssh/ge-jumpbox.pub")}"
+/** key for ehime */
+resource "aws_key_pair" "ehime" {
+  key_name   = "ehime-jumpbox"
+  public_key = "${file("~/.ssh/ehime-jumpbox.pub")}"
 }
